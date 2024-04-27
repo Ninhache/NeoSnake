c44791f048452bb145cecd42312e16aab25d3301
Ninhache <mr.carambar27@gmail.com>
2024-04-27 04:56:50 +0200

    feat: the first logic of the game... It's working but it's pretty trash imo.. But now, I'm able to start "anything" 🤠

---
+++ src/components/GameCanvas.tsx
@@ -0,0 +1,116 @@
+
+import { useEffect, useRef } from "react";
+import { Food, Snake } from "../classes/Entity";
+
+import { Direction } from "../enums";
+
+type Props = {
+    width: number;
+    height: number;
+}
+
+const GameCanvas: React.FC<Props> = ({ width, height }) => {
+
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const snake = useRef(new Snake(null, { x: 10, y: 10 }));
+    
+    // const food = useRef(new Food(null, { x: 15, y: 15 }));
+    const foodsRef = useRef([
+        new Food(null, { x: 10, y: 10 }),
+        new Food(null, { x: 20, y: 20 }),
+        new Food(null, { x: 30, y: 35 })
+    ]);
+
+    const handleKeyPress = (event: KeyboardEvent) => {
+        const { key } = event;
+
+        switch (key) {
+            case "ArrowUp":
+                snake.current.enqueueDirection(Direction.Up);
+                break;
+            case "ArrowDown":
+                snake.current.enqueueDirection(Direction.Down);
+                break;
+            case "ArrowLeft":
+                snake.current.enqueueDirection(Direction.Left);
+                break;
+            case "ArrowRight":
+                snake.current.enqueueDirection(Direction.Right);
+                break;
+        }
+
+    };
+
+    const draw = (ctx: CanvasRenderingContext2D) => {
+        foodsRef.current.forEach(food => {
+            food.draw(ctx);
+        });
+        snake.current.draw(ctx);
+    };
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas?.getContext('2d');
+
+        if (ctx && canvas) {
+            let frameCount = 0;
+            let animationFrameId: number;
+
+            window.addEventListener('keydown', handleKeyPress);
+
+            const render = () => {
+                frameCount++;
+
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                draw(ctx);
+
+                animationFrameId = window.requestAnimationFrame(render);
+
+                if (frameCount % 15 === 0) {
+
+                    foodsRef.current.forEach((food) => {
+                        if (snake.current.head.x === food.coordinates.x && snake.current.head.y === food.coordinates.y) {
+                            snake.current.eat(food);
+                        }
+                    });
+
+                    if (!snake.current.move(Math.floor(width / 20), Math.floor(height / 20))) {
+                        alert('Game Over');
+                        window.cancelAnimationFrame(animationFrameId);
+                    }
+
+                }
+            }
+
+            render();
+
+            return () => {
+                window.cancelAnimationFrame(animationFrameId),
+                    window.removeEventListener('keydown', handleKeyPress);
+            }
+
+        }
+
+        // if (ctx && canvas) {
+        //     const gameLoop = setInterval(() => {
+        //         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        //         snake.current.move();
+        //         food.current.draw(ctx);
+        //         snake.current.draw(ctx);
+        //     }, 100);
+
+        //     window.addEventListener('keydown', handleKeyPress);
+
+        //     return () => {
+        //         clearInterval(gameLoop),
+        //         window.removeEventListener('keydown', handleKeyPress);
+        //     }
+        // }
+
+    }, [draw]);
+
+    return (<canvas ref={canvasRef} width={width} height={height} style={{ border: '1px solid black' }} />);
+
+};
+
+export default GameCanvas;
