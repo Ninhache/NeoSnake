1367e8ea3ac79f23c59887f965e88cc21184c62e
Ninhache <mr.carambar27@gmail.com>
2024-04-27 04:55:29 +0200

    feat: misc.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import GameCanvas from './components/GameCanvas'
+import './styles/App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <GameCanvas width={800} height={800} />
+    </>
+  )
+}
+
+export default App
