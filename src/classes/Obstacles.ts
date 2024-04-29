2b43099f459189665e8df918cf682606710a4148
Ninhache <mr.carambar27@gmail.com>
2024-04-30 00:38:17 +0200

    feat: you can now create map from a Json (will be usefull later ;))

---
+++ src/classes/Obstacles.ts
@@ -0,0 +1,56 @@
+
+
+import { Entity } from "./Entity";
+import { SnakeMap, Tile } from "./Map";
+import { Snake } from "./Snake";
+
+export abstract class Obstacle extends Entity {
+    constructor(tile: Tile) {
+        super(tile);
+    }
+
+    abstract effect(snake: Snake, map: SnakeMap): void;
+    abstract draw(ctx: CanvasRenderingContext2D): void;
+}
+
+abstract class ObstacleDecorator extends Obstacle {
+    protected obstacle: Obstacle;
+
+    constructor(obstacle: Obstacle) {
+        super(obstacle.getLocationTile());
+        this.obstacle = obstacle;
+    }
+
+    public effect(snake: Snake, map: SnakeMap): void {
+        this.obstacle.effect(snake, map);
+    }
+
+    public abstract draw(ctx: CanvasRenderingContext2D): void;
+}
+
+export class BasicObstacle extends Obstacle {
+    public effect(snake: Snake): void {
+        snake.hasToDie = true;
+    }
+
+    public draw(ctx: CanvasRenderingContext2D): void {
+        ctx.fillStyle = "black";
+        ctx.fillRect(this.getLocationTile().x * 20, this.getLocationTile().y * 20, 20, 20);
+    }
+}
+
+export class DifferentObstacle extends ObstacleDecorator {
+    constructor(obstacle: Obstacle) {
+        super(obstacle);
+    }
+
+    public effect(snake: Snake, map: SnakeMap): void {
+        super.effect(snake, map);
+        snake.hasToDie = true;
+    }
+
+    public draw(ctx: CanvasRenderingContext2D): void {
+        ctx.fillStyle = "blue";
+        ctx.fillRect(this.getLocationTile().x * 20, this.getLocationTile().y * 20, 20, 20);
+    }
+}
