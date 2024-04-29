2f9c74b18cf84ba2d691c84b6b4bdce238b7ed5b
Ninhache <mr.carambar27@gmail.com>
2024-04-30 00:38:37 +0200

    fix: update canvasComponent according to the new changes

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -1,115 +1,106 @@
 
 import { useEffect, useRef } from "react";
-import { BasicFood, BigFruit, DeadlyFruit, Food, Snake } from "../classes/Entity";
 
+import jsonMap from "../assets/jsons/testMap.json";
+import { Food } from "../classes/Entity";
+import { SnakeMap } from "../classes/Map";
 import { Direction } from "../enums";
 
 type Props = {
-    width: number;
-    height: number;
+	width: number;
+	height: number;
 }
 
 const GameCanvas: React.FC<Props> = ({ width, height }) => {
 
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-    const snake = useRef(new Snake(null, { x: 10, y: 10 }));
-    
-    // const food = useRef(new Food(null, { x: 15, y: 15 }));
-    const foodsRef = useRef([
-        new BasicFood(null, { x: 10, y: 10 }),
-        new BigFruit(new BasicFood(null, { x: 25, y: 20 })),
-        new BasicFood(null, { x: 30, y: 35 }),
-        new DeadlyFruit(new BasicFood(null, { x: 15, y: 20 }))
-    ]);
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const map = new SnakeMap(JSON.stringify(jsonMap));
+	const snake = map.snake;
 
-    const handleKeyPress = (event: KeyboardEvent) => {
-        // Keep all the events unless they are arrow keys
-        if (
-            ![
-              "ArrowLeft",
-              "ArrowUp",
-              "ArrowRight",
-              "ArrowDown",
-            ].includes(event.key)
-          )
-            return;
-        event.preventDefault();
-        const { key } = event;
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const ctx = canvas?.getContext('2d');
 
-        switch (key) {
-            case "ArrowUp":
-                snake.current.enqueueDirection(Direction.Up);
-                break;
-            case "ArrowDown":
-                snake.current.enqueueDirection(Direction.Down);
-                break;
-            case "ArrowLeft":
-                snake.current.enqueueDirection(Direction.Left);
-                break;
-            case "ArrowRight":
-                snake.current.enqueueDirection(Direction.Right);
-                break;
-        }
-    };
+		const handleKeyPress = (event: KeyboardEvent) => {
+			// Keep all the events unless they are arrow keys
+			if (
+				![
+					"ArrowLeft",
+					"ArrowUp",
+					"ArrowRight",
+					"ArrowDown",
+				].includes(event.key)
+			)
+				return;
+			event.preventDefault();
+			const { key } = event;
 
-    const draw = (ctx: CanvasRenderingContext2D, frameNumber: number) => {
-        foodsRef.current.forEach(food => {
-			food.draw(ctx);
-		});
+			switch (key) {
+				case "ArrowUp":
+					snake.enqueueDirection(Direction.Up);
+					break;
+				case "ArrowDown":
+					snake.enqueueDirection(Direction.Down);
+					break;
+				case "ArrowLeft":
+					snake.enqueueDirection(Direction.Left);
+					break;
+				case "ArrowRight":
+					snake.enqueueDirection(Direction.Right);
+					break;
+			}
+		};
 
-		snake.current.draw(ctx, (frameNumber % 20) / 20);
-    };
+		const draw = (ctx: CanvasRenderingContext2D, frameNumber: number) => {
+			map.draw(ctx, (frameNumber % 20) / 20);
+		};
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const ctx = canvas?.getContext('2d');
+		if (ctx && canvas) {
+			let frameCount = 0;
+			let animationFrameId: number;
 
-        if (ctx && canvas) {
-            let frameCount = 0;
-            let animationFrameId: number;
+			window.addEventListener('keydown', handleKeyPress);
 
-            window.addEventListener('keydown', handleKeyPress);
+			const render = () => {
+				frameCount++;
+				animationFrameId = window.requestAnimationFrame(render);
 
-            const render = () => {
-                frameCount++;
-                animationFrameId = window.requestAnimationFrame(render);
+				ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+				ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-                
-                ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-                ctx.fillRect(0, 0, canvas.width, canvas.height);
-                
-                if (frameCount % 20 === 0) {
+				if (frameCount % 20 === 0) {
 
-                    const snakeHasMoved = snake.current.move(Math.floor(width / 20), Math.floor(height / 20));
+					const snakeHasMoved = snake.processMovement(map);
 
-                    foodsRef.current.forEach((food) => {
-                        if (snake.current.head.x === food.coordinates.x && snake.current.head.y === food.coordinates.y) {
-                            snake.current.eat(food, Math.floor(width / 20), Math.floor(height / 20))
-                        }
-                    });
+					const tile = map.getTile(snake.head);
+					if (tile) {
+						if (tile.data instanceof Food) {
+							snake.eat(tile.data);
+						}
+					}
 
-                    if (!snakeHasMoved) {
-                        alert('Game Over');
-                        window.cancelAnimationFrame(animationFrameId);
-                    }
+					if (!snakeHasMoved) {
+						alert('Game Over');
+						window.cancelAnimationFrame(animationFrameId);
+					}
 
-                }
+				}
 
 				draw(ctx, frameCount);
 
-            }
+			}
 
-            render();
+			render();
 
-            return () => {
-                window.cancelAnimationFrame(animationFrameId),
-                    window.removeEventListener('keydown', handleKeyPress);
-            }
+			return () => {
+				window.cancelAnimationFrame(animationFrameId),
+					window.removeEventListener('keydown', handleKeyPress);
+			}
 
-        }
-    }, []);
+		}
+	}, []);
 
-    return (<canvas ref={canvasRef} width={width} height={height} style={{ border: '1px solid black' }} />);
+	return (<canvas ref={canvasRef} width={width} height={height} style={{ border: '1px solid black' }} />);
 
 };
 
