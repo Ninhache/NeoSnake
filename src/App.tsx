294c3cc3e6e9b595ab82a0891b715a8af0cf643e
Ninhache <mr.carambar27@gmail.com>
2024-04-29 03:14:54 +0200

    feat: improve routes & styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 
 import { Route, BrowserRouter as Router, Routes, } from 'react-router-dom'
-import NavBar from './components/Widgets/WidgetNavbar'
 import WidgetHeader from './components/Widgets/WidgetHeader'
+import NavBar from './components/Widgets/WidgetNavbar'
 
-import './styles/App.css'
-import GameCanvas from './components/GameCanvas'
 import WidgetFooter from './components/Widgets/WidgetFooter'
 import WidgetGame from './components/Widgets/WidgetGame'
+import './styles/App.css'
+import WidgetHome from './components/Widgets/WidgetHome'
+import WidgetFaq from './components/Widgets/WidgetFaq'
 
 const App: React.FC<{ }> = ({ }) => {
   return (
       <>
-      <WidgetHeader />
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<div>blablabla cobra a été fondée en 2024</div>} />
-          <Route path="/play" element={<WidgetGame />} />
-          <Route path="/faq" element={<div>faq</div>} />
-        </Routes>
-      </Router>
-      <WidgetFooter />
+        <div className='flex justify-center items-center flex-col'>
+          <WidgetHeader />
+          <Router>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<WidgetHome />} />
+              <Route path="/play" element={<WidgetGame />} />
+              <Route path="/faq" element={<WidgetFaq />} />
+            </Routes>
+          </Router>
+          <WidgetFooter />
+        </div>
       </>
   )
 }
