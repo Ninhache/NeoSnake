dc2b16bf5e45a60402b26b17030c830d3cd9e5d8
Ninhache <mr.carambar27@gmail.com>
2024-05-06 03:03:15 +0200

    feat: improve types & what the JSON can laod

--- src/@types/MapTypes.ts
+++ src/@types/MapTypes.ts
@@ -1,4 +1,3 @@
-
 import { Coordinates } from "./CoordinatesType";
 import { DirectionType } from "./DirectionType";
 
@@ -7,23 +6,24 @@ export type obstacleType = "OBa" | "ODi";
 export type gameObjectType = foodType | obstacleType;
 
 export interface SnakeMapData {
-    options: {
-        width: number,
-        height: number,
-        cellSize: number
-    },
-    snake: {
-        startPosition: Coordinates,
-        direction: DirectionType,
-        bodyParts: Coordinates[]
-    },
-    gameObject: {
-        x: number,
-        y: number,
-        type: gameObjectType
-    }[]
+  options: {
+    width: 800;
+    height: 800;
+    cellSize: number;
+    name: string;
+  };
+  snake: {
+    startPosition: Coordinates;
+    direction: DirectionType;
+    length: number | 3;
+  };
+  gameObject: {
+    x: number;
+    y: number;
+    type: gameObjectType;
+  }[];
 }
 
 export interface NextFrameInfo {
-    snakeHasMoved: boolean;
+  snakeHasMoved: boolean;
 }
