a17103bd980beac79cf9c985f1f82ce158f4aa60
Ninhache <mr.carambar27@gmail.com>
2024-05-06 17:10:51 +0200

    feat: add the route & the component to start

---
+++ src/components/Widgets/WidgetCreate.tsx
@@ -0,0 +1,7 @@
+import LayoutComponent from "../layouts/LayoutComponent";
+
+const WidgetCreate: React.FC = () => {
+  return <LayoutComponent>Allo neo faut dev là</LayoutComponent>;
+};
+
+export default WidgetCreate;
