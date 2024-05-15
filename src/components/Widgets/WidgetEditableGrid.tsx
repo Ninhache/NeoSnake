15e0af0c232f9fd5486859569daf184c56fc2a3e
Ninhache <mr.carambar27@gmail.com>
2024-05-15 11:49:22 +0200

    feat: now using canvas to improve perfs

--- src/components/Widgets/WidgetEditableGrid.tsx
+++ src/components/Widgets/WidgetEditableGrid.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import UIEditableCell from "../UI/UIEditableCell";
+import { useEffect, useRef, useState } from "react";
 import { useEditor } from "../contexts/EditorContext";
 
 type Props = {
@@ -8,88 +7,140 @@ type Props = {
 };
 
 const WidgetEditableGrid: React.FC<Props> = ({ width, height }) => {
-  const { mapData, addGameObject, deleteGameObject, currentGameObjectType } =
+  const { mapData, currentScenario, isDrawing, addObstacle, deleteObstacle } =
     useEditor();
-  const { cellSize } = mapData.options;
-  const numCols = width / cellSize;
-  const numRows = height / cellSize;
-
-  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [isDragging, setIsDragging] = useState(false);
   const [dragOperation, setDragOperation] = useState<"ADD" | "DELETE" | null>(
     null
   );
 
-  const handleMouseDown = (
-    event: React.MouseEvent<HTMLDivElement>,
-    x: number,
-    y: number
-  ) => {
-    event.preventDefault();
+  const { cellSize } = mapData.options;
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const overlayRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+
+    if (!canvas || !ctx) return;
+
+    if (ctx && canvas) {
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, width, height);
+
+      mapData.maps[currentScenario].obstacles.forEach((obstacle) => {
+        ctx.fillStyle = "black";
+        ctx.fillRect(
+          obstacle.x * cellSize,
+          obstacle.y * cellSize,
+          cellSize,
+          cellSize
+        );
+      });
+
+      mapData.maps[currentScenario].fruits.forEach((fruit) => {
+        ctx.fillStyle = "red";
+        ctx.fillRect(
+          fruit.actualPosition.x * cellSize,
+          fruit.actualPosition.y * cellSize,
+          cellSize,
+          cellSize
+        );
+
+        fruit.futurePosition.forEach((futurePosition) => {
+          //ctx.fillStyle = "pink"
+          ctx.fillStyle = "rgb(255, 193, 203, 0.5)";
+
+          ctx.fillRect(
+            futurePosition.x * cellSize,
+            futurePosition.y * cellSize,
+            cellSize,
+            cellSize
+          );
+        });
+      });
+
+      if (currentScenario === 0) {
+        const { x, y } = mapData.snake.startPosition;
+        ctx.fillStyle = "green";
+        ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+      }
+    }
+  }, [currentScenario, mapData, cellSize]);
+
+  const drawOverlay = (x: number, y: number) => {
+    const ctx = overlayRef.current?.getContext("2d");
+    if (ctx) {
+      ctx.clearRect(0, 0, width, height);
+      ctx.fillStyle = "rgba(255, 255, 0, 0.5)";
+      ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+
+      ctx.strokeStyle = "black";
+      ctx.strokeRect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  };
+
+  const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    const rect = canvasRef.current?.getBoundingClientRect();
+    if (!rect) return;
+    const x = Math.floor((event.clientX - rect.left) / cellSize);
+    const y = Math.floor((event.clientY - rect.top) / cellSize);
     setIsDragging(true);
 
     if (event.button === 0) {
       setDragOperation("ADD");
-      addGameObject({ x, y, type: currentGameObjectType });
+      addObstacle({ x, y, type: "OBa" });
     } else if (event.button === 2) {
       setDragOperation("DELETE");
-      deleteGameObject(x, y);
+      deleteObstacle({ x, y });
     }
-  };
-
-  const handleMouseEnter = (x: number, y: number) => {
-    if (isDragging) {
-      if (dragOperation === "ADD") {
-        addGameObject({ x, y, type: currentGameObjectType });
-      } else if (dragOperation === "DELETE") {
-        deleteGameObject(x, y);
-      }
-    }
-  };
-
-  const handleMouseUp = (event: MouseEvent) => {
     event.preventDefault();
-    setIsDragging(false);
   };
 
-  useEffect(() => {
-    const globalMouseUp = (event: MouseEvent) => {
-      event.preventDefault();
-      handleMouseUp(event);
-    };
+  const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    const rect = canvasRef.current?.getBoundingClientRect();
+    if (!rect) return;
+    const x = Math.floor((event.clientX - rect.left) / cellSize);
+    const y = Math.floor((event.clientY - rect.top) / cellSize);
 
-    if (isDragging) {
-      window.addEventListener("mouseup", globalMouseUp);
-    } else {
-      window.removeEventListener("mouseup", globalMouseUp);
+    drawOverlay(x, y);
+    if (!isDragging) return;
+
+    if (dragOperation === "ADD") {
+      addObstacle({ x, y, type: "OBa" });
+    } else if (dragOperation === "DELETE") {
+      deleteObstacle({ x, y });
     }
+  };
 
-    return () => {
-      window.removeEventListener("mouseup", globalMouseUp);
-    };
-  }, [isDragging]);
+  const handleMouseUp = () => {
+    setIsDragging(false);
+    setDragOperation(null);
+  };
 
   return (
     <div
       onContextMenu={(e) => e.preventDefault()}
-      style={{ width, height, display: "flex", flexWrap: "wrap" }}
+      style={{ position: "relative" }}
     >
-      {Array.from({ length: numRows * numCols }).map((_, index) => {
-        const x = index % numCols;
-        const y = Math.floor(index / numCols);
-        const existingObject = mapData.gameObject.find(
-          (obj) => obj.x === x && obj.y === y
-        );
-        return (
-          <UIEditableCell
-            key={`${x}-${y}`}
-            x={x}
-            y={y}
-            content={existingObject ? existingObject.type : null}
-            onMouseDown={(e) => handleMouseDown(e, x, y)}
-            onMouseEnter={() => handleMouseEnter(x, y)}
-          />
-        );
-      })}
+      {isDrawing && (
+        <canvas
+          width={width}
+          height={height}
+          ref={overlayRef}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+          className="absolute top-0 left-0"
+        />
+      )}
+      <canvas
+        width={width}
+        height={height}
+        ref={canvasRef}
+        className={`${isDrawing ? "cursor-pointer" : ""}`}
+      />
     </div>
   );
 };
