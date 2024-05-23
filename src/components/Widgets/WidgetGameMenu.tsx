3253ac87633ff0be55a932549e28793d59ae7ae3
Ninhache <mr.carambar27@gmail.com>
2024-05-23 04:25:10 +0200

    feat: improve the menu to have more informations

--- src/components/Widgets/WidgetGameMenu.tsx
+++ src/components/Widgets/WidgetGameMenu.tsx
@@ -1,22 +1,53 @@
+import UIColorfulText from "../UI/UIColorfulText";
 import UISpeedButtons from "../UI/UISpeedButtons";
 import { useGame } from "../contexts/GameContext";
 
-type Props = {};
-const WidgetGameMenu: React.FC<Props> = ({}) => {
+const WidgetGameMenu: React.FC = () => {
   const { state } = useGame();
 
   return (
-    <div className="border-2 w-full flex flex-col justify-between">
+    <div className="w-full flex flex-col justify-between bg-gray-900 bg-opacity-30 ml-4 rounded-lg">
       <div>
-        <h1 className="text-center text-2xl">{state.name}</h1>
+        <h1 className="text-center text-2xl my-4">{state.name}</h1>
 
-        <h1>Score : {state.score}</h1>
-        <h1>Level : {state.level}</h1>
+        <div className="border-2 mx-16 my-4 border-opacity-45 border-gray-500"></div>
 
-        <br></br>
+        <div className="flex justify-center gap-4">
+          <h1>Level : {state.level}</h1>
+          <h1>Score : {state.score} </h1>
+        </div>
 
-        <UISpeedButtons />
+        <div className="border-2 mx-16 my-4 border-opacity-45 border-gray-500"></div>
+
+        <p className="text-center text-lg font-bold text-gray-400">
+          How to play ?
+        </p>
+
+        <ul className="m-4 text-justify text-gray-500">
+          <li>- Use the arrow keys to move the snake.</li>
+          <li>
+            - Eat all the visible food to grow and avoid hitting the walls or
+            yourself.
+          </li>
+          <li>- Once you eat all the food, you will move to the next level.</li>
+          <li>- The game will end once you complete all the levels.</li>
+        </ul>
+
+        <div className="border-2 mx-16 my-4 border-opacity-45 border-gray-500"></div>
+
+        <p className="text-center text-lg font-bold text-gray-400">
+          Additional Controls
+        </p>
+
+        <p className="m-4 text-justify text-gray-500">
+          Press R to restart the game.
+        </p>
+
+        <div className="border-2 mx-16 my-4 border-opacity-45 border-gray-500"></div>
+
+        {window.secretMode && <UIColorfulText text="Secret Mode Activated" />}
       </div>
+      <UISpeedButtons />
     </div>
   );
 };
