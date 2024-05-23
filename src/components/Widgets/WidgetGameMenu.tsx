05bd5459f4f3842a877dac20324eaf91f0fabb78
Ninhache <mr.carambar27@gmail.com>
2024-05-24 00:25:23 +0200

    feat: add question mark that redirect to an article

--- src/components/Widgets/WidgetGameMenu.tsx
+++ src/components/Widgets/WidgetGameMenu.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import UIColorfulText from "../UI/UIColorfulText";
 import UISpeedButtons from "../UI/UISpeedButtons";
 import { useGame } from "../contexts/GameContext";
@@ -6,7 +7,27 @@ const WidgetGameMenu: React.FC = () => {
   const { state } = useGame();
 
   return (
-    <div className="w-full flex flex-col justify-between bg-gray-900 bg-opacity-30 ml-4 rounded-lg">
+    <div className="relative w-full flex flex-col justify-between bg-gray-900 bg-opacity-30 ml-4 rounded-lg">
+      <NavLink
+        to="/article/how-to-play-neosnake"
+        className="absolute top-0 right-0 p-2 text-gray-600 hover:text-gray-400 opacity-40 hover:opacity-100 transition duration-300 ease-in-out"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 scale-150 fill-gray-200 stroke-gray-700"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
+          />
+        </svg>
+      </NavLink>
+
       <div>
         <h1 className="text-center text-2xl my-4">{state.name}</h1>
 
